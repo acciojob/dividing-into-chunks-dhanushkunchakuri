81156a198cac21b7c12de2348d96e4c40a3332c6
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-05 17:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let curr=[];
+	let cursum;
+	let res=[];
+
+	for(let item of arr){
+		if(cursum + item <= n){
+			curr.push(item);
+			cursum+=item;
+		}
+		else{
+			if(curr.length >0){
+				res.push(curr);
+			}
+			curr=[item];
+			cursum=item;
+		}
+	}
+	if(curr.length > 0){
+		res.push(curr);
+	}
+	return res;
 };
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
+
+
